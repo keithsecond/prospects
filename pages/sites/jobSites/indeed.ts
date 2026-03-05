8444d9129226c0d40a2ecafbd11bff00ed861b01
Keith S <terminaltrillness@gmail.com>
2026-03-05 12:40:34 -0600

    cdp poc, add paths

--- pages/sites/jobSites/indeed.ts
+++ pages/sites/jobSites/indeed.ts
@@ -1,5 +1,5 @@
 import { test, expect } from '@playwright/test';
-import { SpecialContextPage } from '../../../classes/specialContextPage';
+import { SpecialContextPage } from '@classes/specialContextPage';
 
 export class IndeedPage extends SpecialContextPage {
 
---
+++ tests/uaWebdriver.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect } from '@playwright/test';
+import { SpecialContextPage } from '@classes/specialContextPage';
+
+test.describe('UA Webdriver Tests', () => {
+    let specialContextPage: SpecialContextPage;
+
+    test('test goto', async ({ browser, page }) => {
+        specialContextPage = new SpecialContextPage(browser, page);
+        await specialContextPage.noNavigator();
+        await specialContextPage.page.goto('https://bot.sannysoft.com/');
+        await page.screenshot({ path: 'test-data/screenshotNoNav.png' });
+});
+
+    test('test goto1', async ({ browser, page }) => {
+        specialContextPage = new SpecialContextPage(browser, page);
+        await specialContextPage.cdpBrowser();
+        await specialContextPage.page.goto('https://bot.sannysoft.com/');
+        await page.screenshot({ path: 'test-data/screenshotCDP.png' });
+    })
+});
