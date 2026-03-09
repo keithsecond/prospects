f535334f51eee6968612402225459d033792b6e9
Keith S <terminaltrillness@gmail.com>
2026-03-09 16:11:57 -0500

    JSON collector added as a utility
    Case BURN-0003: move job collector to a utility class

--- tests/burnettSearch.spec.ts
+++ tests/burnettSearch.spec.ts
@@ -1,8 +1,8 @@
-import { test, expect, Locator } from '@fixtures/burnett';
-import fs from 'fs';
-import path from 'path';
+import { test, expect } from '@fixtures/burnett';
+import { Utilities } from '@classes/utilities';
 
 test.describe('Burnett Tests', () => {
+    const utils = new Utilities();
     const jobQA = 'quality assurance';
     const jobDesktop = 'desktop support';
     const jobSupport = 'technical support';
@@ -14,40 +14,10 @@ test.describe('Burnett Tests', () => {
         test (`search ${term}`, async ({ burnettSearch }) => {
             const container = burnettSearch.resultContainer;
             const jobs = burnettSearch.jobs;
-
             await burnettSearch.search();
             await expect(container, 'search produced results').toBeVisible();
-            const allJobs: Locator[] = await jobs.all();
-            await jobLoop(allJobs);
+            const allJobs = await jobs.all();
+            await utils.newJobsWriteJSON(allJobs, 'Burnett');
         })
     }
 });
-
-async function jobLoop(allJobs: Locator[]) {
-    const filePath = path.join(__dirname, 'jobResults.json');
-    const fileData = fs.readFileSync(filePath, 'utf-8');
-    const data = JSON.parse(fileData);
-    const existingJobIds = new Set(data.Burnett.jobs.map((job: { id: string; }) => job.id));
-
-    for (const jobWeb of allJobs) {
-        const jobTitle = await jobWeb.innerText();
-        const link = await jobWeb.getAttribute('href');
-        if (link == '#') {
-            return;
-        }
-        if (link !== null) {
-            const jobID = link.split('/').slice(-1)[0];
-            const scrapedJobs = {
-                id: jobID,
-                title: jobTitle,
-                status: '0',
-                date: new Date().toISOString()
-            };
-            if (!existingJobIds.has(scrapedJobs.id)) {
-                data.Burnett.jobs.push(scrapedJobs);
-                existingJobIds.add(scrapedJobs.id);
-            }
-        }
-        fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
-        }
-}
